d850cbd25e0b10314cb09681cca748047a3bac25
Ismaël Qouiqa <qouiqaismail@gmail.com>
2025-01-10 17:05:33 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,12 +25,7 @@
 #
 # For more on node definitions, see: https://puppet.com/docs/puppet/latest/lang_node_definitions.html
 node 'ip-172-31-32-167.eu-west-3.compute.internal' {
-  include nginx
-  
-  nginx::resource::server { 'kibana.myhost.com':
-    listen_port => 80,
-    proxy       => 'http://ismaelqouiqa.com/proxy',
-  }
+  contain nginx
 }
 
 node default {
