71dea7e1e237eb96be59a5f18d644f5a6e0ad334
Ismaël Qouiqa <qouiqaismail@gmail.com>
2025-01-11 16:56:17 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -24,10 +24,11 @@
 # Puppet Enterprise console and External Node Classifiers (ENC's).
 #
 # For more on node definitions, see: https://puppet.com/docs/puppet/latest/lang_node_definitions.html
-$puppetdb_host = '172.31.6.210'
+
 
 #Puppet server
 node 'ip-172-31-10-8.eu-west-3.compute.internal'{
+  $puppetdb_host = 'ec2-15-236-134-100.eu-west-3.compute.amazonaws.com'
   class { 'puppetdb::master::config':
     puppetdb_server => $puppetdb_host,
   }
