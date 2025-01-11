4fb09a880fc6c27d02640afb2bafae951cb2e2a3
Ismaël Qouiqa <qouiqaismail@gmail.com>
2025-01-11 18:46:33 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -55,6 +55,12 @@ node 'ip-172-31-12-94.eu-west-3.compute.internal'{
 node 'ip-172-31-8-131.eu-west-3.compute.internal'{
   # Here we install and configure PuppetDB, and tell it where to
   # find the PostgreSQL database.
+   service { 'iptables':
+     ensure => 'stopped',
+   }
+   service { 'ip6tables':
+     ensure => 'stopped',
+   }
   class { 'puppetdb::server':
     database_host => $postgres_host,
   }
