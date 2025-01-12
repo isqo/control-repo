4cefb590dd914b83c22663a6da5d0a6dae9b0bf2
Ismaël Qouiqa <qouiqaismail@gmail.com>
2025-01-12 09:17:50 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -33,7 +33,7 @@
       docker::run { 'puppetboard':
         image => 'ghcr.io/voxpupuli/puppetboard',
         env   => [
-          'PUPPETDB_HOST=ec2-15-188-83-177.eu-west-3.compute.amazonaws.com',
+          'PUPPETDB_HOST='ip-172-31-8-131.eu-west-3.compute.internal',
           'PUPPETDB_PORT=8081',
           'PUPPETBOARD_PORT=8088',
           'SECRET_KEY=9d71211996c7aad729da68a49edd61e33209860f68aa4d168e062b31f88e20df',
