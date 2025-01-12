9cc85075050b1f81f870d420a157a84894e1d91b
Ismaël Qouiqa <qouiqaismail@gmail.com>
2025-01-12 09:52:22 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,19 +25,6 @@
 #
 # For more on node definitions, see: https://puppet.com/docs/puppet/latest/lang_node_definitions.html
  node 'ip-172-31-10-8.eu-west-3.compute.internal'{
-      include docker
-      docker::image { 'ghcr.io/voxpupuli/puppetboard': }
-      $puppetdb_host='ip-172-31-8-131.eu-west-3.compute.internal'
-      docker::run { 'puppetboard':
-        image => 'ghcr.io/voxpupuli/puppetboard',
-        env   => [
-          'PUPPETDB_HOST=$puppetdb_host',
-          'PUPPETDB_PORT=8081',
-          'PUPPETBOARD_PORT=8088',
-          'SECRET_KEY=9d71211996c7aad729da68a49edd61e33209860f68aa4d168e062b31f88e20df',
-        ],
-        net   => 'host',
-      }
       class { 'puppetdb::master::config':
         puppetdb_server => $puppetdb_hostst,
       }
