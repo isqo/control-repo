c57989a4b627b32b8d3cdb4d351a81bc53e86329
Ismaël Qouiqa <qouiqaismail@gmail.com>
2025-01-12 11:15:05 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -117,7 +117,7 @@ node 'ip-172-31-32-167.eu-west-3.compute.internal' {
   class { 'apache': }
 }
 
-node 'ip-172-31-42-30.eu-west-3.compute.internal' {
+node 'ip-172-31-32-239.eu-west-3.compute.internal' {
 Exec { path => "/usr/sbin" }
 
   package { 'java-17-openjdk':
